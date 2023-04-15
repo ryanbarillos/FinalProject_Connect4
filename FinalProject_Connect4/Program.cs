6b70b9695c481c8c6b6053473a91f7fde733e85d
Computer Developer <Computer Developer@WINVM>
2023-04-15 14:34:45 -0700

    Add project files.

---
+++ FinalProject_Connect4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinalProject_Connect4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
