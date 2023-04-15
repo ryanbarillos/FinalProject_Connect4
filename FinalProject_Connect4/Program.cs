cfaf3c3e360ba2258d3187ac3bfcb5873d100aa0
Computer Developer <Computer Developer@WINVM>
2023-04-15 14:49:45 -0700

    Update noveau

--- FinalProject_Connect4/Program.cs
+++ FinalProject_Connect4/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
-
+/* 
+ * Final Project, Connect 4
+ * by Ryan Barillos, 439090
+ * 
+ * Date Started: 15 Apr 2023
+ * Day Finished: 21 Apr 2023 (hopefully)
+ */
 namespace FinalProject_Connect4
 {
     internal class Program
     {
+        public class Player
+        {
+            public int  PlayerWins = 0,
+                        PlayerLosses = 0;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
